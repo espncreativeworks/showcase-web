b1735aef792f14974193f8118bfa98004d490897
Chris Montoro <chris.montoro@espn.com>
2015-03-03 12:02:42 -0500

    conform to .jshint

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -10,7 +10,7 @@ router.post('/', function(req, res, next) {
   passport.authenticate('local', function (err, account, info) {
     var error = err || info;
     if (error) return res.json(401, error);
-    if (!account) return res.json(404, {message: 'Something went wrong, please try again.'});
+    if (!account) return res.json(404, { message: 'Something went wrong, please try again.' });
 
     var token = auth.signToken(account._id, account.role);
     res.json({ token: token });
