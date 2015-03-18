dcecd85b252f0abe5ea08ed233ab3b2506918fb8
skparkk@gmail.com <skparkk@gmail.com>
2015-03-18 11:07:13 -0400

    Adding in detail footer for projects

--- client/app/projects/detail/projects.detail.controller.js
+++ client/app/projects/detail/projects.detail.controller.js
@@ -13,15 +13,17 @@ angular.module('espnCreativeworksShowcaseApp')
     };
 
     $scope.project = Project.get({ id: $stateParams.id });
-
+    
     $scope.project.$promise.then(function (project){
       Page.meta.title = project.meta.title || (project.title + ' Project Details');
       Page.meta.description = project.meta.description || fullDescriptionFilter(project.description, { plaintext: true });
       $scope.project.hero.file.transformedUrl = $.cloudinary.url($scope.project.hero.file.public_id, { secure: true, transformation: 'project_detail_hero' });
-      $scope.project.$executions = Project.executions({ id: project._id });
+      
+      $scope.project.$executions = Project.executions({ id: project._id }); 
       return $scope.project.$executions.$promise;
+      
     }).then(function (executions){
-      $scope.project.$tags = []; 
+      $scope.project.$tags = [];
 
       angular.forEach(executions, function (execution){
         angular.forEach(execution.tags, function (tag){
@@ -34,9 +36,21 @@ angular.module('espnCreativeworksShowcaseApp')
 
         angular.forEach(execution.videos, function (video){
           video.embed.trustedHtml = $sce.trustAsHtml(video.embed.html)
-        });
-
+        });   
       });
+      
+      $scope.project.$related = $scope.project.related;
+      return $scope.project.$related.$promise;
+        
+    }).then(function(){
+    	$scope.project.$related = [];
+    	
+    	for(var i = 0; i < $scope.project.related.length; i++){
+    	   $scope.project.$rel = Project.get({ id: $scope.project.related[i] });
+    	   $scope.project.$related.push($scope.project.$rel);
+    	   console.log($scope.project.$rel);
+    	}
+    	
     });
     
   }]);
--- client/app/projects/filter/projects.filter.controller.spec.js
+++ client/app/projects/filter/projects.filter.controller.spec.js
@@ -10,7 +10,7 @@ describe('Controller: ProjectsFilterCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    ProjectsFilterCtrl = $controller('ProjectsListCtrl', {
+    ProjectsFilterCtrl = $controller('ProjectsFilterCtrl', {
       $scope: scope
     });
   }));
