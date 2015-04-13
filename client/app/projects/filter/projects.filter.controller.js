a8749ef3457d58eacd73d59682fe67c3f5350c34
Chris Montoro <chris.montoro@espn.com>
2015-04-13 13:59:50 -0400

    Optimize project list performance - reduce HTTP requests by performing population logic on the server - support new endpoint `/projects/search` - use smaller, transformed images in project-card directive

--- client/app/projects/filter/projects.filter.controller.js
+++ client/app/projects/filter/projects.filter.controller.js
@@ -10,83 +10,41 @@ angular.module('espnCreativeworksShowcaseApp')
     $scope.industries = [];
     $scope.tags = [];
 
-    var _projects
-      , _brandsResolved = false
-      , _executionsResolved = false;
-
-    function onbrandsresolved (evt, filteredProjects){
-      _brandsResolved = true;
-
-      var _brands = []
-        , _industries = [];
-
-      angular.forEach(filteredProjects, function (project){
-        
-        // add brand to list on scope
-        _brands = _brands.concat(project.brands);
-
-        angular.forEach(project.brands, function (brand){
-          // add industries to list on scope
-          _industries = _industries.concat(brand.industries);
-        });
-      });
-
-      $scope.brands = _.uniq(_brands, function (b){ return b._id; });
-      $scope.industries = _.uniq(_industries, function (i){ return i._id; });
-    }
-
-    function onexecutionsresolved (evt, filteredProjects){
-      _executionsResolved = true;
-
-      var _executions = []
+    function onprojectsupdated (evt, filteredProjects){
+      var _sports = []
+        , _brands = []
+        , _industries = []
+        , _executions = []
         , _platforms = []
         , _tags = [];
 
       angular.forEach(filteredProjects, function (project){
+        
+        _sports = _sports.concat(project.sports);
+        _brands = _brands.concat(project.brands);
+        _industries = _industries.concat(project.industries);
         _executions = _executions.concat(project.executions);
 
+        angular.forEach(project.brands, function (brand){
+          _industries = _industries.concat(brand.industries);
+        });
+
         angular.forEach(project.executions, function (execution){
-          // add platform to list on scope
           _platforms.push(execution.platform);
           _tags = _tags.concat(execution.tags);
         });
 
       });
 
-      // add executions to list on scope
+      $scope.sports = _.uniq(_sports, function (s){ return s._id; });
+      $scope.brands = _.uniq(_brands, function (b){ return b._id; });
+      $scope.industries = _.uniq(_industries, function (i){ return i._id; });
       $scope.executions = _.uniq(_executions, function (e){ return e._id; });
-
-      // add platform to list on scope
       $scope.platforms = _.uniq(_platforms, function (p){ return p._id; });
-
-      // add tags to list on scope
       $scope.tags = _.uniq(_tags, function (t){ return t._id; });
-
     }
 
-    $scope.$on('projects:filtered', function (evt, filteredProjects){
-      _projects = filteredProjects;
-
-      var _sports = [];
-      
-      angular.forEach(filteredProjects, function (project){
-        _sports = _sports.concat(project.sports);
-      });
-
-      $scope.sports = _.uniq(_sports, function (s){ return s._id; });
-
-      if (_brandsResolved){
-        onbrandsresolved(evt, filteredProjects);
-      }
-
-      if (_executionsResolved){
-        onexecutionsresolved(evt, filteredProjects);
-      }
-
-    });
-
-    $scope.$on('brands:resolved', onbrandsresolved);
-    $scope.$on('executions:resolved', onexecutionsresolved);
+    $scope.$on('projects:updated', onprojectsupdated);
 
     $scope.addFilter = function addFilter(category, data){
       var exists = false;
--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,12 +5,12 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: process.env.MONGO_URI || 'mongodb://localhost/espn-creativeworks-showcase-dev'
+    uri: process.env.MONGO_URI || 'mongodb://localhost/espn-creativeworks-showcase'
   },
 
   cms: {
     uri: process.env.CMS_URI || 'http://localhost:3000/api'
   },
 
-  seedDB: true
+  seedDB: false
 };
