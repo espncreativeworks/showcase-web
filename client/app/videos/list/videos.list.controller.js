b920f490b0fc89e2a75310797b5d3a94d4874a5d
Chris Montoro <chris.montoro@espn.com>
2015-05-29 14:46:41 -0400

    implement media detail views; keep list/detail views consistent

--- client/app/videos/list/videos.list.controller.js
+++ client/app/videos/list/videos.list.controller.js
@@ -1,6 +1,25 @@
 'use strict';
 
 angular.module('espnCreativeworksShowcaseApp')
-  .controller('VideosListCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('VideosListCtrl', ['$scope', 'underscore', 'Page', 'Video', function ($scope, _, Page, Video) {
+  	Page.meta.title = 'Videos';
+  	Page.meta.description = 'Videos from ESPN CreativeWorks';
+  	Page.meta.keywords = 'espn creativeworks videos, espn creative works videos';
+  	Page.body.class = 'video-list';
+
+    $scope.videos = Video.query();
+    $scope.videos.$promise.then(function (videos){
+    	$scope.$videos = _.chain(videos)
+                        .filter(function (video){
+                          return video.usage === 'execution' && video.platform;
+                        })
+                        .sortBy(function (videos){
+                          return videos.embed.thumbnailHeight * -1;
+                        })
+                        .groupBy(function (video){
+                          return video.platform.name;
+                        })
+                        .value();
+      console.log($scope.$videos);
+    })
+  }]);
