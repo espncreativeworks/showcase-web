cfc693b11333fa57cb92401f28137c33bf165028
kate.park@espn.com <skparkk@gmail.com>
2015-10-16 14:18:43 -0400

    get rid of console log to prepare to move to prod

--- client/app/collections/detail/collections.detail.controller.js
+++ client/app/collections/detail/collections.detail.controller.js
@@ -21,19 +21,19 @@ angular.module('espnCreativeworksShowcaseApp')
     } 
 
     $scope.collection.$promise.then(function (collection){
-      console.log("collection: ", collection);
-      console.log("collection creator: ", collection.creator);
+      // console.log("collection: ", collection);
+      // console.log("collection creator: ", collection.creator);
       collection.creator.$displayName = collection.creator.name.indexOf(' ') >= 0 ? collection.creator.name.split(' ')[0] : collection.creator.name;
       collection.creator.$profileImageUrl = $.cloudinary.url('default_profile_image'); // jshint ignore:line
       Page.meta.title = collection.meta.title || (collection.title + ' Collection Details');
 
       if (Auth.isLoggedIn()) {
-        console.log("logged in!");
+        // console.log("logged in!");
         $scope.getCurrentAccount = Auth.getCurrentAccount;
-        console.log("account name: " + $scope.getCurrentAccount().name + " creator name: " + collection.creator.name);
+        // console.log("account name: " + $scope.getCurrentAccount().name + " creator name: " + collection.creator.name);
 
         if ($scope.getCurrentAccount().name == collection.creator.name) {
-          console.log("names are the same!");
+          // console.log("names are the same!");
           $scope.canEdit = true;
         }
       }
@@ -74,7 +74,7 @@ angular.module('espnCreativeworksShowcaseApp')
     });
 
     $scope.showNote = function(item) {
-      console.log("showNote trigger: ", item);
+      // console.log("showNote trigger: ", item);
       // console.log("e.current target: ", e);
       // console.log($('.collection-item-form-container.'+item.slug));
 
@@ -83,12 +83,12 @@ angular.module('espnCreativeworksShowcaseApp')
     };
     $scope.updateCollectionItem = function(formName, item) {
       // console.log(formName);
-      console.log("update collection item trigger: ", item);
+      // console.log("update collection item trigger: ", item);
       CollectionItem.update({_id: item._id, notes: item.notes});
     }
 
     $scope.deleteItem = function(item) {
-      console.log("delete item: ", item);
+      // console.log("delete item: ", item);
       // console.log("delete item.id: ", item._id);
       CollectionItem.destroy({id: item._id});
       window.location.reload();
